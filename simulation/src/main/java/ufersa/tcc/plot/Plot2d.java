69eadad2094e89591b0e60f6342e244f38a61afb
Juan Carlos <juanksoliveira@gmail.com>
2018-08-30 01:30:19 -0300

    Classe modelo para plot2d

--- simulation/src/main/java/ufersa/tcc/plot/Plot2d.java
+++ simulation/src/main/java/ufersa/tcc/plot/Plot2d.java
@@ -1,45 +1,73 @@
 package ufersa.tcc.plot;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
+import java.io.IOException;
 
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.BitmapEncoder.BitmapFormat;
-import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
 import org.knowm.xchart.style.Styler.ChartTheme;
-import org.knowm.xchart.style.markers.Marker;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
 public class Plot2d {
-	public static void main(String[] args) throws Exception {
-		String nome = "Sample Chart";
-		double[] xData = new double[] { 0.0, 1.0, 2.0 };
-		double[] yData = new double[] { 2.0, 1.0, 0.0 };
 
+	/** Nome do plot */
+	private String name;
+	private XYChart chart;
+
+	/**
+	 * Construtor principal.
+	 */
+	public Plot2d(String name, int width, int height, String xDataLabel, double xData[], String yDataLabel,
+			double yData[]) {
+		this.name = name;
 		// Criação do Gráfico
-		XYChart chart = new XYChartBuilder().width(800).height(600).theme(ChartTheme.Matlab).title("Matlab Theme")
-				.xAxisTitle("X").yAxisTitle("Y").build();
+		this.chart = new XYChartBuilder().width(width).height(height).theme(ChartTheme.Matlab).title(name)
+				.xAxisTitle(xDataLabel).yAxisTitle(yDataLabel).build();
 
 		// Estilização do Gráfico
-		// chart.getStyler().setPlotGridLinesVisible(false);
-		chart.getStyler().setPlotGridHorizontalLinesVisible(true);
+		chart.getStyler().setPlotGridLinesVisible(false);
 		chart.getStyler().setXAxisTickMarkSpacingHint(100);
 
 		// Adicionando série para plot - Sem marcador
-		chart.addSeries(nome, xData, yData).setMarker(SeriesMarkers.NONE);
+		chart.addSeries(name, xData, yData).setMarker(SeriesMarkers.NONE);
 
-		// Adicionando série para plot - Com marcador (quadrado)
-		chart.addSeries(nome + 2, xData, yData).setMarker(SeriesMarkers.RECTANGLE);
-		chart.getSeriesMap().get(nome).setLineColor(Color.CYAN);
+	}
 
-		// Salvando em arquivo
-		BitmapEncoder.saveBitmapWithDPI(chart, "./" + nome, BitmapFormat.PNG, 300);
+	/**
+	 * @see Plot2d#Plot2d(String, int, int, String, double[], String, double[])
+	 */
+	public Plot2d(String name, String xDataLabel, double xData[], String yDataLabel, double yData[]) {
+		this(name, 800, 600, xDataLabel, xData, yDataLabel, yData);
+	}
+
+	/**
+	 * Mostrar plot na tela.
+	 */
+	public void display() {
 
 		// Mostrar em tela
 		new SwingWrapper(chart).displayChart();
-
 	}
+
+	/**
+	 * Salva o plot em arquivo. Formato: PNG, 300 DPI.
+	 */
+	public void saveFile(String src) throws IOException {
+		BitmapEncoder.saveBitmapWithDPI(chart, src + this.name, BitmapFormat.PNG, 300);
+	}
+
+	/**
+	 * @see Plot2d#saveFile(String)
+	 */
+	public void saveFile() throws IOException {
+		this.saveFile("./");
+	}
+
+	// GETTERS AND SETTERS
+	public XYChart getChart() {
+		return this.chart;
+	}
+
 }
